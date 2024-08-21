4f68629779a286a1f2dbbe46b538faa771613cac
kyllesubrian <subriankylle@gmail.com>
2024-08-21 10:53:43 +0800

    Create TableAndChairs.java

---
+++ Chapter 1/TableAndChairs.java
@@ -0,0 +1,11 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        
+        System.out.println("O                      O");
+        System.out.println("0      OOOOOOOOOO      O");
+        System.out.println("00000  0        0  00000");
+        System.out.println("0   O  0        O  O   O");
+        System.out.println("O   O  O        O  O   O");
+
+    }
+}
