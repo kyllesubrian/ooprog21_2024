fdbc9ddf97d7d8c139368d4e245e4ad0578e0e49
kyllesubrian <subriankylle@gmail.com>
2024-08-21 10:49:43 +0800

    Create MovieQuoteInfo.java

---
+++ Chapter 1/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+public class MovieQuoteInfo{
+
+public static void main(String[] args){
+
+System.out.println("Quote: My Name Is Barry Allen. I'm The Fastest Man Alive. ”);
+System.out.println("Movie: The Flash. ");
+System.out.println("Actor: Grant Gustin. ");
+System.out.println("Year: 2013 ");
+
+}
+}
